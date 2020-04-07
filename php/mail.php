d604fcf3e6a8b1043050146b035e6fdd554477bc
AmadeusW0lfe <serwillow.wolfe@gmail.com>
2020-04-07 04:11:49 +0300

    изменено:      php/mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,11 +1,9 @@
 
 <?php
 	
-	$milo = htmlspecialchars($_POST['email']);
-	#$message;
-	#$message = wordwrap($message, 70, "\r\n");
 
-mail('serwillow.wolfe@gmail.com', 'My Subject', $milo);
+
+mail('serwillow.wolfe@gmail.com', 'My Subject', $_POST['maile']);
 ?>
 
 
