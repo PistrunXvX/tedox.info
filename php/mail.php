1bc27af0b3bc62d2fa0cfe26990840ef1bcc9b82
AmadeusW0lfe <serwillow.wolfe@gmail.com>
2020-04-07 03:44:05 +0300

    изменено:      php/mail.php

--- php/mail.php
+++ php/mail.php
@@ -2,8 +2,8 @@
 <?php
 	
 	$var1=$_POST['email'];
-	$message;
-	$message = wordwrap($message, 70, "\r\n");
+	#$message;
+	#$message = wordwrap($message, 70, "\r\n");
 
 mail('serwillow.wolfe@gmail.com', 'My Subject', $var1);
 ?>
