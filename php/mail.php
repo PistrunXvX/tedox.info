83ce7711b42fe565df4e5c3ff9eecef3436b430b
AmadeusW0lfe <serwillow.wolfe@gmail.com>
2020-04-07 05:56:09 +0300

    изменено:      php/mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,6 +1,7 @@
 <?php
-$message = $_POST['maile'];
-$message2 = $_POST['phonenumber'];
-mail('serwillow.wolfe@gmail.com', 'My Subject', $message, $message2);
+	$message = $_POST['maile'];
+	$message2 = $_POST['phonenumber'];
+
+	mail('serwillow.wolfe@gmail.com', 'My Subject', "Почта: ".$message."\n", "Телефон: ".$message2."\n");
 ?>
 
